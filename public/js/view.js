8c2701cf676081b79b97aeb95a97f6d32bd5cd14
Jay Rodriguez <jared.m.rodriguez@gmail.com>
2020-05-27 17:52:22 -0500

    prelim updates to config/connection & some basic public js/html files

---
+++ public/js/view.js
@@ -0,0 +1,67 @@
+// When user hits the search-btn
+$("#search-btn").on("click", function (event) {
+    event.preventDefault();
+
+    // Save the book they typed into the book-search input
+    var itemSearched = $("#item-search").val().trim();
+
+    // Make an AJAX get request to our api, including the user's book in the url
+    $.get("/api/" + itemSearched, function (data) {
+        console.log(data);
+
+        // Call our renderBooks function to add our books to the page
+        renderItems(data);
+    });
+});
+
+// When user hits the genre-search-btn
+$("#category-search-btn").on("click", function () {
+
+    // Save the book they typed into the genre-search input
+    var categorySearched = $("#category-search").val().trim();
+
+    // Make an AJAX get request to our api, including the user's genre in the url
+    $.get("/api/category/" + categorySearched, function (data) {
+        console.log(data);
+        // Call our renderBooks function to add our books to the page
+        renderItems(data);
+    });
+});
+
+function renderItems(data) {
+    if (data.length !== 0) {
+
+        $("#stats").empty();
+        $("#stats").show();
+
+        for (var i = 0; i < data.length; i++) {
+
+            var div = $("<div>");
+//////////update pending API Response information
+            // div.append("<h2>" + data[i].title + "</h2>");
+            // div.append("<p>Author: " + data[i].author + "</p>");
+            // div.append("<p>Genre: " + data[i].genre + "</p>");
+            // div.append("<p>Pages: " + data[i].pages + "</p>");
+            // div.append("<button class='delete' data-id='" + data[i].id + "'>DELETE BOOK</button>");
+
+            $("#stats").append(div);
+
+        }
+
+        // $(".delete").click(function () {
+
+        //     $.ajax({
+        //         method: "DELETE",
+        //         url: "/api/book/" + $(this).attr("data-id")
+        //     })
+        //         // On success, run the following code
+        //         .then(function () {
+        //             console.log("Deleted Successfully!");
+        //         });
+
+        //     $(this).closest("div").remove();
+
+        // });
+
+    }
+}
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -24,3 +24,71 @@ module.exports = function(app) {
     });
   });
 };
+
+// *********************************************************************************
+// api-routes.js - this file offers a set of routes for displaying and saving data to the db
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var Ebay = require("../models/ebay.js");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+  // Get all items
+  app.get("/api/all", function(req, res) {
+    Ebay.findAll({}).then(function(results) {
+      res.json(results);
+    });
+  });
+
+  // Get a specific item
+  app.get("/api/:ebay", function(req, res) {
+    Ebay.findAll({
+      where: {
+        //name: req.params.item
+      }
+    }).then(function(results) {
+      res.json(results);
+    });
+  });
+
+  // Get all items of a specific genre
+  app.get("/api/category/:category", function(req, res) {
+    Ebay.findAll({
+      where: {
+        category: req.params.category
+      }
+    }).then(function(results) {
+      res.json(results);
+    });
+  });
+
+  // // Add a item
+  // app.post("/api/new", function(req, res) {
+  //   console.log("Ebay Data:");
+  //   console.log(req.body);
+  //   Ebay.create({
+  //     title: req.body.title,
+  //     author: req.body.author,
+  //     genre: req.body.genre,
+  //     pages: req.body.pages
+  //   }).then(function(results) {
+  //     res.json(results);
+  //   });
+  // });
+
+  // // Delete a item
+  // app.delete("/api/item/:id", function(req, res) {
+  //   console.log("item ID:");
+  //   console.log(req.params.id);
+  //   Ebay.destroy({
+  //     where: {
+  //       id: req.params.id
+  //     }
+  //   }).then(function() {
+  //     res.end();
+  //   });
+  // });
+};
